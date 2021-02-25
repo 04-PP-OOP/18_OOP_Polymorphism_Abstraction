2c23ca2dd1934c7312529dcd7c4665cd6a3b8f51
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-02-25 09:52:03 +0100

    Add files via upload

---
+++ 06_OOP_1_ Polymorphism/com/cc/java/Start.java
@@ -0,0 +1,35 @@
+package com.cc.java;
+
+public class Start {
+
+	public static void main(String[] args) {
+		
+		HoneyBee queen = new Queen();
+		HoneyBee worker = new Worker();
+		HoneyBee drone = new Drone();
+		
+		ausgabe(queen.doYourJob());
+		ausgabe(worker.doYourJob());
+		ausgabe(drone.doYourJob());
+		
+		ausgabe("-----------------");
+		
+		ausgabe(queen.fly());
+		ausgabe(worker.fly());
+		ausgabe(drone.fly());
+		
+		ausgabe("-----------------");
+		
+		// Das sollte eigentlich nicht möglich sein
+		// --> Lösung: Abstrakte Klasse
+		HoneyBee bee = new HoneyBee();
+		ausgabe(bee.doYourJob());
+
+	}
+	
+	
+	protected static void ausgabe(String outStr) {
+		System.out.println(outStr);
+	}
+
+}
